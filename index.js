efbb627388341a26a024f54c60c0043b4106c974
zhchy <>
2019-07-31 20:01:33 +0800

    支持按目录结构输出

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ tinify.key = config.key;
 
 init();
 function init() {
-  compressAll(readSourceImg());
+  compressAll(readSourceImg(config.sourceDir));
 }
 
 async function compressAll(imgList) {
@@ -29,9 +29,11 @@ async function compress(file) {
   for(let i = 0; i < num; i++) {
     const beforeSize = fs.lstatSync(sourcePath).size;
     // let outPath = path.join(config.tinyDir, `${file.name}-${i + 1}.png`);
-    let outPath = path.join(config.tinyDir, `${file.name}.png`);
+    // let outPath = path.join(config.tinyDir, `${file.name}.png`);
+    let outPath = file.path.replace(new RegExp(config.sourceDir), config.tinyDir)
     let source = tinify.fromFile(sourcePath);
     console.log(`压缩第${i+1}次`)
+    dirExists(path.join(outPath, '..'));
     await source.toFile(outPath);
     const tinySize = fs.lstatSync(outPath).size;
     finalSize = tinySize;
@@ -46,19 +48,78 @@ async function compress(file) {
 }
 
 // 读取源文件
-// readSourceImg();
-function readSourceImg() {
-  const files = fs.readdirSync(config.sourceDir)
-  const list = [];
+// console.log(readSourceImg(config.sourceDir));
+function readSourceImg(imgPath) {
+  const files = fs.readdirSync(imgPath)
+  let list = [];
 
   files.forEach(file => {
-    let obj = {
-      path: path.join(config.sourceDir, file),
+    const oPath = path.join(imgPath, file);
+    if (fs.lstatSync(oPath).isDirectory()) {
+      list = list.concat(readSourceImg(oPath));
+    } else {
+      let obj = {
+        path: oPath,
+      }
+      let arr = file.split('.');
+      if (/png|jpg|jpeg/i.test(arr.pop())) {
+        obj.name = arr.join('.');
+        list.push(obj);
+      }
     }
-    let arr = file.split('.');
-    arr.pop();
-    obj.name = arr.join('.');
-    list.push(obj);
   });
   return list;
+}
+
+
+async function dirExists(dir) {
+  let isExists = await getStat(dir);
+  //如果该路径且不是文件，返回true
+  if (isExists && isExists.isDirectory()) {
+    return true;
+  } else if (isExists) {     //如果该路径存在但是文件，返回false
+    return false;
+  }
+  //如果该路径不存在
+  let tempDir = path.parse(dir).dir;      //拿到上级路径
+  //递归判断，如果上级目录也不存在，则会代码会在此处继续循环执行，直到目录存在
+  let status = await dirExists(tempDir);
+  let mkdirStatus;
+  if (status) {
+    mkdirStatus = await mkdir(dir);
+  }
+  return mkdirStatus;
+}
+
+
+/**
+ * 读取路径信息
+ * @param {string} path 路径
+ */
+function getStat(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(stats);
+      }
+    })
+  })
+}
+
+/**
+ * 创建路径
+ * @param {string} dir 路径
+ */
+function mkdir(dir) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(dir, err => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    })
+  })
 }
